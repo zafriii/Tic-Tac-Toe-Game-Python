729cadbe081998878d63fce0ba69dc66d3e4b49f
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-05-08 00:06:43 +0600

    Update HumanvsAI.py

--- HumanvsAI.py
+++ HumanvsAI.py
@@ -89,15 +89,15 @@ class TicTacToe:
             return best_score
 
     def check_winner(self):
-        # Check rows
+        
         for row in self.board:
             if row[0] == row[1] == row[2] != "":
                 return True
-        # Check columns
+        
         for col in range(3):
             if self.board[0][col] == self.board[1][col] == self.board[2][col] != "":
                 return True
-        # Check diagonals
+      
         if self.board[0][0] == self.board[1][1] == self.board[2][2] != "":
             return True
         if self.board[0][2] == self.board[1][1] == self.board[2][0] != "":
